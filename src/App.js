78694877f2620491dcb7ac921ff7451363aefe51
“Rob <rjgleave@amazon.com>
2020-06-05 14:52:34 -0400

    push a change

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Rob Edit <code>src/App.js</code> and save to reload ***
+            Hello Rob Edit <code>src/App.js</code> and save to reload ***
           </p>
           <a
             className="App-link"
